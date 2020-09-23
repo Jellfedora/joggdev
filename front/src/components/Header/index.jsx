3b3c90cf2dbc16505f4ef8bbab46842b15b16dd1
Jellfedora <julienlecointe@live.fr>
2020-09-23 15:32:14 +0200

    Added simple header

---
+++ front/src/components/Header/index.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import {
+    Link
+} from "react-router-dom";
+import { connect } from 'react-redux';
+
+class Header extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+    }
+
+    componentDidMount() {
+    }
+
+    render() {
+        return (
+            <div className="header">
+                <h1>JoggDev_</h1>
+            </div>
+        );
+    }
+}
+const mapDispatchToProps = (dispatch) => {
+    return {
+        dispatch: (action) => { dispatch(action) }
+    }
+}
+const mapStateToProps = (state) => {
+    console.log(state);
+    return {
+    }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
+
